325cf9ee344de64585572f4b9642ba7922321f9c
DEVARASHETTY NITHISH CHANDRA AZAD <nithishchandra16@gmail.com>
2025-10-28 16:27:52 +0530

    Change app port from 5070 to 5060

--- api.py
+++ api.py
@@ -196,4 +196,4 @@ def health(sample) -> Response:
 # Run App
 # -----------------------------
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5070)
+    app.run(debug=True, host='0.0.0.0', port=5060)
